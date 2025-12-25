975cd6fa5c1f39b0d001c2304422cc32f050f28c
Romashko_nastya <romashkonastya5@gmail.com>
2025-12-25 23:12:06 +0300

    solve task 2

--- zad-1.js
+++ zad-1.js
@@ -3,4 +3,11 @@ let x = 20;
 let y = 58;
 let z = 42;
 console.log(x + y + z);
-
+//zad2
+let secInMin = 60;
+let minInHour = 60;
+let hourInDay = 24;
+let dayInYear = 365;
+let myAge = 55;
+let myAgeInSeconds = myAge * dayInYear * hourInDay * minInHour * secInMin;
+console.log(myAgeInSeconds);
