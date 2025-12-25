c50e0483ca6613ba7d1d61b6590415af2424990c
Romashko_nastya <romashkonastya5@gmail.com>
2025-12-25 23:12:49 +0300

    solve task 3

--- zad-1.js
+++ zad-1.js
@@ -11,3 +11,15 @@ let dayInYear = 365;
 let myAge = 55;
 let myAgeInSeconds = myAge * dayInYear * hourInDay * minInHour * secInMin;
 console.log(myAgeInSeconds);
+//zad3
+let count = 42;
+let userName = '42';
+let countString = count + '';
+let countString2 = count.toString();
+console.log(countString, typeof (countString));
+console.log(countString2, typeof (countString2));
+let userNameNumber = +userName;
+let userNameNumber2 = parseInt(userName);
+console.log(userNameNumber, typeof (userNameNumber));
+console.log(userNameNumber2, typeof (userNameNumber2));
+
