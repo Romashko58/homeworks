84db902240537f74dfebb85e248b8012633c3ae4
Romashko_nastya <romashkonastya5@gmail.com>
2026-01-11 18:36:23 +0300

    task 1 completed

---
+++ lesson-17.js
@@ -0,0 +1,11 @@
+//zad1
+const a = 'true';
+const b = false;
+const c = 17;
+const d = undefined;
+const e = null;
+console.log(typeof (a));
+console.log(typeof (b));
+console.log(typeof (c));
+console.log(typeof (d));
+console.log(typeof (e));
