4ef5654cba512770fe23a79451174c89c71c9932
Romashko_nastya <148887709+Romashko58@users.noreply.github.com>
2026-01-18 20:02:37 +0300

    fix task 4 (lesson-17.js)

--- lesson-17.js
+++ lesson-17.js
@@ -30,7 +30,7 @@ const documents = true;
 const pen = true;
 const apple = false;
 const orange = true;
-const shouldGoToWork = pen ?? documents ?? pen ?? (apple || orange);
+const shouldGoToWork = pen && documents && pen && (apple || orange);
 console.log(shouldGoToWork);
 //zad5
 const chislo = prompt('Введите число');
