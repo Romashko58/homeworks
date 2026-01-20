80815a91be24abef51bb5fcdc15f4c0892c6ed7e
Romashko_nastya <romashkonastya5@gmail.com>
2026-01-21 00:04:30 +0300

    task 3 completed

--- lesson-20.js
+++ lesson-20.js
@@ -4,3 +4,10 @@ console.log(colors.length);
 //task2
 const animals = ['monkey', 'dog', 'cat']
 console.log(animals[animals.length - 1]);
+//task3
+const numbers = [5, 43, 63, 23, 90];
+numbers.splice(0, numbers.length);
+console.log(numbers);
+const numbers1 = [5, 43, 63, 23, 90];
+numbers1.length = 0;
+console.log(numbers1);
