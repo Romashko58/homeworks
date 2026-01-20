70a090efc7b708eb091db278188a56d9def145cf
Romashko_nastya <romashkonastya5@gmail.com>
2026-01-20 18:33:49 +0300

    task 2 completed

--- lesson-18.js
+++ lesson-18.js
@@ -7,5 +7,17 @@ function getSum(number) {
    return result;
 }
 console.log(getSum(100));
+//task2
+function overpaymentByCredit(sum, percent) {
+   const month = 60;
+   const paymantMonth = sum / month;
+   let res = 0;
+   for (let i = 1; i <= month; i++) {
+      res += sum * percent / 12;
+      sum -= paymantMonth;
+   }
+   return res;
+}
+console.log(overpaymentByCredit(6000, 0.17))
 
 
