5c3362524bcee962cce2b320cfb84249f2ba34f2
Romashko_nastya <romashkonastya5@gmail.com>
2026-01-21 00:05:40 +0300

    task 5 completed

--- lesson-20.js
+++ lesson-20.js
@@ -18,3 +18,11 @@ students.push('Borya');
 students.shift();
 students.unshift('Andrey');
 console.log(students);
+//task5
+const cats = ['Gachito', 'Tom', 'Batman']
+for (let i = 0; i < cats.length; i++) {
+   console.log(cats[i]);
+}
+for (const cat of cats) {
+   console.log(cat);
+}
