b8547738dee26170bf982c2633407c93567b96a0
Romashko_nastya <romashkonastya5@gmail.com>
2026-01-20 20:51:44 +0300

    task 2 completed

--- lesson-19.js
+++ lesson-19.js
@@ -7,3 +7,12 @@ console.log(user);
 delete user.firstName;
 delete user.lastName;
 console.log(user);
+//task2
+const person = {
+   name: 'Elena',
+   email: 'elena@gmail.com'
+};
+if ('name' in person) {
+   console.log(true);
+}
+
