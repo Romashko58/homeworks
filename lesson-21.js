2559737ad096fa1b9288f9e3f9df22f061fa22d2
Romashko_nastya <romashkonastya5@gmail.com>
2026-01-29 13:09:41 +0300

    task 1 completed

---
+++ lesson-21.js
@@ -0,0 +1,9 @@
+//task1
+const fibonacci = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987];
+fibonacci.forEach(function (item) {
+   console.log(item);
+});
+fibonacci.forEach(item => {
+   console.log(item);
+});
+const users = ['Darya', 'Masha', 'Denis', 'Vitaliy', 'Polina', 'Anton'];
