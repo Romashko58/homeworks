31a3dd283e94ecd58281f629560b860afe7fd679
Romashko_nastya <romashkonastya5@gmail.com>
2026-01-29 13:13:04 +0300

    task 1(advanced level) completed

--- lesson-21.js
+++ lesson-21.js
@@ -42,3 +42,37 @@ console.log(numberFind);
 const numberFind1 = numbers2.find(function (num) {
    return num % 2 === 0;
 });
+//task1(advanced level)
+function Student(name, salary, rate) {
+   this.name = name;
+   this.salary = salary;
+   this.rate = rate;
+
+   this.getCreditAmount = function () {
+      switch (this.rate) {
+         case 'A':
+            return this.salary * 12;
+         case 'B':
+            return this.salary * 9;
+         case 'C':
+            return this.salary * 6;
+         case 'D':
+            return 0;
+         default:
+            return 0;
+      }
+   };
+}
+const student1 = new Student('Ivan', 1000, 'A');
+const student2 = new Student('Svetlana', 500, 'D');
+const student3 = new Student('Sasha', 150, 'C');
+const student4 = new Student('Nastya', 1200, 'B');
+const student5 = new Student('Pavel', 800, 'D');
+const students = [student1, student2, student3, student4, student5];
+function getTotalCreditAmount(students) {
+   return students.reduce((total, student) => {
+      return total + student.getCreditAmount();
+   }, 0);
+}
+const totalCredit = getTotalCreditAmount(students);
+console.log(totalCredit);
