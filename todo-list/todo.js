30f62256b6e18ca5cf526424cd99bbfc80472ca2
Romashko_nastya <148887709+Romashko58@users.noreply.github.com>
2026-02-08 16:45:21 +0300

    add function createElementTodo and rename div

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,95 +1,61 @@
+function createElementTodo(tagName, atributes = {}, textContent = '') {
+   const element = document.createElement(tagName);
+   if (atributes.className) {
+      element.className = atributes.className;
+   }
+   if (textContent) {
+      element.textContent = textContent;
+   }
+   return element;
+}
+
+
 const divRoot = document.getElementById('root');
 const fragment = document.createDocumentFragment();
-const divEnter = document.createElement('div');
-divEnter.classList.add('enter');
-
-const btnDeleteAll = document.createElement('button');
-btnDeleteAll.classList.add('button');
-btnDeleteAll.textContent = 'Delete All';
-
-const btnDeleteLast = document.createElement('button');
-btnDeleteLast.classList.add('button');
-btnDeleteLast.textContent = 'Delete Last';
-
-const inputEnterTodo = document.createElement('input');
-inputEnterTodo.classList.add('input');
+const managementElements = createElementTodo('div', { className: 'enter' });
+const btnDeleteAll = createElementTodo('button', { className: 'button' }, 'Delete All');
+const btnDeleteLast = createElementTodo('button', { className: 'button' }, 'Delete Last');
+const inputEnterTodo = createElementTodo('input', { className: 'input' },)
 inputEnterTodo.placeholder = 'Enter todo ...';
+const btnAdd = createElementTodo('button', { className: 'button' }, 'Add');
+managementElements.append(btnDeleteAll, btnDeleteLast, inputEnterTodo, btnAdd);
 
-const btnAdd = document.createElement('button');
-btnAdd.classList.add('button');
-btnAdd.textContent = 'Add';
-divEnter.append(btnDeleteAll, btnDeleteLast, inputEnterTodo, btnAdd);
-
-const divShow = document.createElement('div');
-divShow.classList.add('show');
-
-const todoAll = document.createElement('span');
-todoAll.classList.add('spanStats');
-todoAll.textContent = 'All: '
-const allNumber = document.createElement('span');
-allNumber.textContent = '0';
+const filterElements = createElementTodo('div', { className: 'show' });
+const todoAll = createElementTodo('span', { className: 'spanStats' }, 'All: ');
+const allNumber = createElementTodo('span', {}, '0');
 todoAll.append(allNumber);
 
-const todoCompleted = document.createElement('span');
-todoCompleted.classList.add('spanStats');
-todoCompleted.textContent = 'Completed: '
-const completedNumber = document.createElement('span');
-completedNumber.textContent = '0';
+const todoCompleted = createElementTodo('span', { className: 'spanStats' }, 'Completed: ');
+const completedNumber = createElementTodo('span', {}, '0');
 todoCompleted.append(completedNumber);
-
-const btnShowAll = document.createElement('button');
-btnShowAll.classList.add('button', 'show-btn');
-btnShowAll.textContent = 'Show All';
-
-const btnShowCompleted = document.createElement('button');
-btnShowCompleted.classList.add('button', 'show-btn');
-btnShowCompleted.textContent = 'Show Completed';
-
-const inputSearchTodo = document.createElement('input');
-inputSearchTodo.classList.add('input', 'search-input');
+const btnShowAll = createElementTodo('button', { className: 'button show-btn' }, 'Show All');
+const btnShowCompleted = createElementTodo('button', { className: 'button show-btn' }, 'Show Completed');
+const inputSearchTodo = createElementTodo('input', { className: 'input search-input' });
 inputSearchTodo.placeholder = 'Search ...';
-divShow.append(todoAll, todoCompleted, btnShowAll, btnShowCompleted, inputSearchTodo);
+filterElements.append(todoAll, todoCompleted, btnShowAll, btnShowCompleted, inputSearchTodo);
 
 
-function createTodoItem(textContent, dateContent) {
-   const todoElement = document.createElement('div');
-   todoElement.classList.add('todoElement');
 
-   const label = document.createElement('label');
-   label.classList.add('custom-checkbox');
-
-   const checkbox = document.createElement('input');
+function createTodoItem(textContent, dateContent, idContent) {
+   const todoElement = createElementTodo('div', { className: 'todoElement' });
+   const label = createElementTodo('label', { className: 'custom-checkbox' });
+   const checkbox = createElementTodo('input', {});
    checkbox.type = 'checkbox';
-
-   const checkmark = document.createElement('span');
-   checkmark.classList.add('checkmark');
-
+   const checkmark = createElementTodo('span', { className: 'checkmark' });
    label.append(checkbox, checkmark);
 
-   const todoText = document.createElement('div');
-   todoText.classList.add('todoText');
-   const text = document.createElement('p');
-   text.classList.add('text');
-   text.textContent = textContent;
+   const todoText = createElementTodo('div', { className: 'todoText' });
+   const text = createElementTodo('p', { className: 'text' }, textContent);
    todoText.append(text);
-
-   const div = document.createElement('div');
-   div.classList.add('closeDiv');
-   const closeBtn = document.createElement('button');
-   closeBtn.classList.add('button', 'close-btn');
-   closeBtn.textContent = 'X';
-   const divDate = document.createElement('div');
-   divDate.classList.add('dateDiv');
-   const date = document.createElement('p');
-   date.classList.add('date');
-   date.textContent = dateContent;
-   divDate.append(date);
-   div.append(closeBtn, divDate);
+   const div = createElementTodo('div', { className: 'closeDiv' });
+   const closeBtn = createElementTodo('button', { className: 'button close-btn' }, 'X');
+   const date = createElementTodo('p', { className: 'date' }, dateContent);
+   todoElement.dataset.id = idContent;
+   div.append(closeBtn, date);
    todoElement.append(label, todoText, div);
    return todoElement;
 }
-const newTodo = createTodoItem('Todo text', '30.01.2026');
-const secondTodo = createTodoItem('Todo text', '01.02.2026');
 
-fragment.append(divEnter, divShow, newTodo, secondTodo);
+const todoListContainer = createElementTodo('div', { className: 'todoListContainer' });
+fragment.append(managementElements, filterElements, todoListContainer);
 divRoot.append(fragment);
