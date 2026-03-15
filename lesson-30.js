375213d655ce3b902ff7f4ef2424158e06e6d101
Romashko_nastya <romashkonastya5@gmail.com>
2026-03-15 11:42:27 +0300

    implement timer application

---
+++ lesson-30.js
@@ -0,0 +1,22 @@
+const timer = document.getElementById('timer');
+const timerValue = document.getElementById('sec');
+timerValue.textContent = `00 sec`;
+const startBtn = document.getElementById('start-btn');
+const pauseBtn = document.getElementById('pause-btn');
+let timerId = null;
+let timerSec = 0;
+
+startBtn.addEventListener('click', () => {
+   if (timerId === null) {
+      timerId = setInterval(() => {
+         timerSec += 1;
+         timerValue.textContent = `${String(timerSec).padStart(2, '0')} sec`;
+         console.log(timerSec);
+      }, 1000);
+   }
+});
+
+pauseBtn.addEventListener('click', () => {
+   clearInterval(timerId);
+   timerId = null;
+})
